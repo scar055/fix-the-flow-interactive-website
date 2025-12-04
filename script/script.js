214317677af21008412d1497283e9eab22c8594b
Rowan <48519289+scar055@users.noreply.github.com>
2025-12-04 12:06:09 +0100

    change name of const #14

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
-// stap 1:  selecteer de chat button 
-const chatButton = document.querySelector('.ai-button')
+// stap 1:  selecteer de chat button
+const openChatButton = document.querySelector(".ai-button");
 // stap 2:  wacht tot iemand er op klikt
-// stap 3:  verander de class van mijn chat venster, zodat deze zichtbaar wordt 
-chatButton.addEventListener('click', function () {
-    document.querySelector('section article').classList.add('visible-chat')
-})
+// stap 3:  verander de class van mijn chat venster, zodat deze zichtbaar wordt
+openChatButton.addEventListener("click", function () {
+  document.querySelector("section article").classList.add("visible-chat");
+});
